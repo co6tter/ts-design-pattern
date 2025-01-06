f815d5826833b583797e65e547cb1394946349c2
co6tter <co6tter@gmail.com>
2025-01-07 08:55:41 +0900

    feat: add observer

---
+++ src/observer.ts
@@ -0,0 +1,24 @@
+class Observable {
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  observers: any[];
+
+  constructor() {
+    this.observers = [];
+  }
+
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  subscribe(func: any) {
+    this.observers.push(func);
+  }
+
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  unsubscribe(func: any) {
+    this.observers = this.observers.filter((observer) => observer !== func);
+  }
+
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  notify(data: any) {
+    // biome-ignore lint/complexity/noForEach: <explanation>
+    this.observers.forEach((observer) => observer(data));
+  }
+}
